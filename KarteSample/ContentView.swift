c31dc08360bab83f01f0f7c9d13a464872249ebd
taicho-nv <taicho.nv@gmail.com>
2022-08-26 15:24:33 +0900

    Initial Commit

---
+++ KarteSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KarteSample
+//
+//  Created by 今村圭佑 on 2022/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KarteSample/KarteSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarteSampleApp.swift
+//  KarteSample
+//
+//  Created by 今村圭佑 on 2022/08/26.
+//
+
+import SwiftUI
+
+@main
+struct KarteSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
